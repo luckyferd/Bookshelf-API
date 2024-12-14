d6e25bee60b458ea3b4c0e1df445df22d0e062d9
Sky <luckyferdiansyah31@gmail.com>
2024-12-14 17:38:13 +0700

    fix getBookHandlerById

--- src/handler.js
+++ src/handler.js
@@ -65,22 +65,23 @@ const getAllBooksHanlder = () =>({
 });
 
 const getAllBooksByIdHandler=(request, h)=>{
-  const { bookId } = request.params;
-  const book= books.find((n)=> n.id===bookId);
+  const { id } = request.params;
+  const book= books.filter((n)=> n.id===id)[0];
 
-  if (!book){
+  if (book !== undefined){
     return h.response({
-      status: 'fail',
-      message : 'Buku tidak ditemukan',
-    }).code(404);
+      status: 'success',
+      data : { book,
+      },
+    }).code(200);
   }
 
-  return {
-    status:'success',
-    data : {
-      book,
-    },
-  };
+  const response =h.response({
+    status :'fail',
+    message:'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
 };
 
 const updateBookByIdHandler=(request, h)=>{
