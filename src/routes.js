d04868703c61db4589b65b079f63028aaf1ec084
Sky <luckyferdiansyah31@gmail.com>
2024-12-14 17:11:36 +0700

    fix getAllBookHandler

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,35 @@
-const{ addBookHandler,
-    getAllBooksHanlder,
-    getAllBooksByIdHandler,
-    updateBookByIdHandler,
-    deleteBookByIdHandler
- }=require('./handler');
+const { addBookHandler,
+  getAllBooksHanlder,
+  getAllBooksByIdHandler,
+  updateBookByIdHandler,
+  deleteBookByIdHandler
+}=require('./handler');
 
- const routes=[
-    {method:'POST',
-        path:'/books',
-        handler :addBookHandler
-    },
-    {
-        method:'GET',
-        path:'/books',
-        handler: getAllBooksHanlder
-    },
-    {
-        method:'GET',
-        path:'/books/{bookId}',
-        handler: getAllBooksByIdHandler
-    },
-    {
-        method:'PUT',
-        path:'/books/{bookId}',
-        handler: updateBookByIdHandler
-    },
-    {
-        method:'DELETE',
-        path:'/books/{bookId}',
-        handler: deleteBookByIdHandler
-    }
- ]
+const routes=[
+  { method:'POST',
+    path:'/books',
+    handler :addBookHandler
+  },
+  {
+    method:'GET',
+    path:'/books',
+    handler: getAllBooksHanlder
+  },
+  {
+    method:'GET',
+    path:'/books/{bookId}',
+    handler: getAllBooksByIdHandler
+  },
+  {
+    method:'PUT',
+    path:'/books/{bookId}',
+    handler: updateBookByIdHandler
+  },
+  {
+    method:'DELETE',
+    path:'/books/{bookId}',
+    handler: deleteBookByIdHandler
+  }
+];
 
- module.exports=routes;
+module.exports=routes;
--- src/server.js
+++ src/server.js
@@ -2,20 +2,19 @@ const Hapi =require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async ()=>{
-    const server = Hapi.server({
-        port:9000,
-        host: 'localhost',
-        /**process.env.NODE_ENV !=='production' ?'localhost':'0.0.0.0',
-        routes:{
-            cors:{
-            origin: ['*'],
-        },**/
+  const server = Hapi.server({
+    port:9000,
+    host: process.env.NODE_ENV !=='production' ?'localhost':'0.0.0.0',
+    routes:{
+      cors:{
+        origin: ['*'],
+      },
     },
-);
-    server.routes(routes);
+  });
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 init();
 
